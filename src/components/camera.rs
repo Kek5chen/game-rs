596b0ef028ccf0fdbd688147f3b16e192b56d433
Kek5chen <kx@imkx.dev>
2024-05-17 10:45:32 +0200

    remove values available in transform

--- src/components/camera.rs
+++ src/components/camera.rs
@@ -5,23 +5,13 @@ use crate::components::Component;
 use crate::object::GameObject;
 
 pub struct CameraComp {
-    projection: Matrix4<f32>,
-    view: Matrix4<f32>,
-    model: Matrix4<f32>,
-    position: Vector3<f32>,
-    rotation: Vector3<f32>,
-    scale: Vector3<f32>,
+    pub projection: Matrix4<f32>,
 }
 
 impl Component for CameraComp {
     fn new() -> Self {
         CameraComp {
             projection: Matrix4::identity(),
-            view: Matrix4::identity(),
-            model: Matrix4::identity(),
-            position: Vector3::zero(),
-            rotation: Vector3::zero(),
-            scale: Vector3::zero(),
         }
     }
 
