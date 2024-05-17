c88edd13552a47a75b15c18dfd192317330597cc
Kek5chen <kx@imkx.dev>
2024-05-17 22:50:08 +0200

    feat: change gravity more granular

--- src/components/gravity.rs
+++ src/components/gravity.rs
@@ -1,18 +1,20 @@
 use crate::components::Component;
 use crate::object::GameObject;
+use cgmath::Vector3;
 use std::cell::RefCell;
 use std::rc::Rc;
-use cgmath::Vector3;
 
 pub struct GravityComp {
-    acceleration: f32,
+    pub acceleration_per_sec: f32,
+    velocity: f32,
     max_acceleration: f32,
 }
 
 impl Component for GravityComp {
     fn new() -> Self {
         GravityComp {
-            acceleration: 0.0,
+            acceleration_per_sec: 8.41,
+            velocity: 0.0,
             max_acceleration: 100.0,
         }
     }
@@ -20,8 +22,9 @@ impl Component for GravityComp {
     fn init(&mut self) {}
 
     fn update(&mut self, parent: Rc<RefCell<GameObject>>, delta_time: f32) {
-        self.acceleration = (self.acceleration - 8.41 * delta_time).clamp(-self.max_acceleration, self.max_acceleration);
+        self.velocity = (self.velocity - self.acceleration_per_sec * delta_time)
+            .clamp(-self.max_acceleration, self.max_acceleration);
         let transform = &mut parent.borrow_mut().transform;
-        transform.translate(Vector3::new(0.0, self.acceleration, 0.0));
+        transform.translate(Vector3::new(0.0, self.velocity, 0.0));
     }
 }
