33fdfb8298856f9c59db93bd2c2d88e2a5132c71
Kek5chen <kx@imkx.dev>
2024-05-31 13:06:07 +0200

    fix: clean up collider a bit

--- src/components/collider.rs
+++ src/components/collider.rs
@@ -5,18 +5,9 @@ use crate::components::{Component, RigidBodyComponent};
 use crate::object::GameObject;
 use crate::world::World;
 
-#[derive(Clone)]
-pub enum ColliderType {
-    Sphere(f32),         // radius
-    Cube(f32, f32, f32), // width, height, depth
-    Mesh(MeshId),
-}
-
 pub struct Collider3D {
-    coll_type: ColliderType,
     pub phys_handle: ColliderHandle,
     linked_to_body: Option<RigidBodyHandle>,
-    collider: Collider,
     parent: *mut GameObject,
 }
 
@@ -25,18 +16,16 @@ impl Component for Collider3D {
     where
         Self: Sized,
     {
-        let coll_type = ColliderType::Cube(1.0, 1.0, 1.0);
-        let collider = Self::default_collider(&coll_type);
+        let shape = SharedShape::cuboid(1.0, 1.0, 1.0);
+        let collider = Self::default_collider(shape);
         let phys_handle = World::instance()
             .physics
             .collider_set
             .insert(collider.clone());
 
         Collider3D {
-            coll_type,
             phys_handle,
             linked_to_body: None,
-            collider,
             parent,
         }
     }
@@ -74,34 +63,8 @@ impl Collider3D {
             .get_mut(self.phys_handle)
     }
 
-    pub fn reshape(&mut self, coll_type: ColliderType) {
-        let collider = Self::default_collider(&coll_type);
-
-        self.collider = collider;
-
-        let world = World::instance();
-        // remove old
-        world.physics.collider_set.remove(
-            self.phys_handle,
-            &mut world.physics.island_manager,
-            &mut world.physics.rigid_body_set,
-            false,
-        );
-
-        // insert new
-        self.phys_handle = world.physics.collider_set.insert(self.collider.clone());
-
-        if let Some(h_body) = self.linked_to_body {
-            self.link_to_rigid_body(Some(h_body));
-        }
-    }
-
-    fn default_collider(coll_type: &ColliderType) -> Collider {
-        match coll_type {
-            ColliderType::Sphere(radius) => ColliderBuilder::ball(*radius),
-            ColliderType::Cube(x, y, z) => ColliderBuilder::cuboid(*x, *y, *z),
-            ColliderType::Mesh(_) => ColliderBuilder::ball(1.0), // FIXME: actual mesh collider
-        }
+    fn default_collider(shape: SharedShape) -> Collider {
+        ColliderBuilder::new(shape)
         .density(1.0)
         .restitution(1.0)
         .build()
