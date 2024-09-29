324bc9d7b1115cf2c3c11eaafb3a8c6606f2b908
Kek5chen <kx@imkx.dev>
2024-09-29 18:41:02 +0200

    fix: camera compound position first needs to be multiplied first

--- src/world.rs
+++ src/world.rs
@@ -82,7 +82,7 @@ impl World {
 
     pub fn new_camera(&mut self) -> GameObjectId {
         let mut obj = self.new_object("Camera");
-        obj.transform.set_compound_pos_first(false);
+        obj.transform.set_compound_pos_first(true);
 
         obj.add_component::<CameraComp>();
 
